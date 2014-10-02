28abcb45e51cb08f9ecf830bf6f8ed02a0cc0b27
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-03 08:09:31 +0900

    Update gh-pages

--- scripts/script.js
+++ scripts/script.js
@@ -3,35 +3,54 @@
 
   Loading = (function() {
     function Loading() {
-      this.r = 10;
+      this.maxR = 10;
+      this.minR = 0;
       this.num = 3;
-      this.padding = this.r * 1.7;
+      this.padding = this.maxR * 1.7;
       this.fill = 'white';
       this.duration = 450;
-      this._start();
+      this.interval = this.duration;
+      this.icons = [];
+      this._initialize();
     }
 
-    Loading.prototype._start = function() {
-      var c, height, i, s, width, _i, _ref, _results;
-      width = this.r * 2 * this.num + (this.padding - this.r) * (this.num - 1);
-      height = this.r * 2;
-      s = Snap(width, height);
+    Loading.prototype.start = function() {
+      return setInterval((function(_this) {
+        return function() {
+          var c, i, _i, _len, _ref, _results;
+          _ref = _this.icons;
+          _results = [];
+          for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+            c = _ref[i];
+            _results.push(_this._timer(c, i));
+          }
+          return _results;
+        };
+      })(this), this.duration * 2 + this.interval);
+    };
+
+    Loading.prototype._initialize = function() {
+      var height, i, width, _i, _ref, _results;
+      width = this.maxR * 2 * this.num + (this.padding - this.maxR) * (this.num - 1);
+      height = this.maxR * 2;
+      this.s = Snap(width, height);
       _results = [];
       for (i = _i = 0, _ref = this.num; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-        c = s.circle(this.r * (i + 1) + this.padding * i, this.r, 0).attr({
-          fill: this.fill
-        });
-        _results.push(this._timer(c, i));
+        _results.push(this.icons.push(this._circle(i)));
       }
       return _results;
+    };
+
+    Loading.prototype._circle = function(i) {
+      return this.s.circle(this.maxR * (i + 1) + this.padding * i, this.maxR, this.minR).attr({
+        fill: this.fill
+      });
     };
 
     Loading.prototype._timer = function(el, i) {
       return setTimeout((function(_this) {
         return function() {
-          return setInterval(function() {
-            return _this._animate(el);
-          }, _this.duration * 2);
+          return _this._animate(el);
         };
       })(this), this.duration / this.num * i);
     };
@@ -41,12 +60,12 @@
       toSmall = (function(_this) {
         return function() {
           return el.animate({
-            r: 0
+            r: _this.minR
           }, _this.duration, mina.linear);
         };
       })(this);
       return el.animate({
-        r: this.r
+        r: this.maxR
       }, this.duration, mina.linear, toSmall);
     };
 
@@ -55,9 +74,9 @@
   })();
 
   window.onload = function() {
-    return setTimeout(function() {
-      return new Loading;
-    }, 1000);
+    var loading;
+    loading = new Loading;
+    return loading.start();
   };
 
 }).call(this);
